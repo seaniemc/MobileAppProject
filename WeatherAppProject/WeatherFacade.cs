f1840d6eef8b5cd46601136dee379bec05dbc76e
Sean McGrath <Sean McGrath>
2016-10-13 21:43:58 +0100

    added the WeatherFacade class with the method GetWeatherLatLon

---
+++ WeatherAppProject/WeatherFacade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAppProject.Model;
+
+namespace WeatherAppProject
+{
+    public class WeatherFacade
+    {
+        public async static Task<GeolookupRootObject> GetWeatherLatlon(double lat, double lon)
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync("http://api.wunderground.com/api/817ffb35035be408/geolookup/q/37.776289,-122.395234.json");
+            var jsonMessage = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(GeolookupRootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            var result = (GeolookupRootObject)serializer.ReadObject(ms);
+
+            return result;
+
+        }
+
+    }
+}
