54d22790ef5b4d5c2dc13d002f9708b894a52d36
Sean McGrath <Sean McGrath>
2016-11-25 18:18:21 +0000

    Got hambuger working

--- WeatherAppProject/MainPage.xaml.cs
+++ WeatherAppProject/MainPage.xaml.cs
@@ -29,38 +29,72 @@ namespace WeatherAppProject
         {
             this.InitializeComponent();
             ReadInFile = new CountryCodesVM("CountryCodes");
+
+            this.InitializeComponent();
+            //BackButton.Visibility = Visibility.Collapsed;
+            ////MyFrame.Navigate(typeof(Financial));
+            //TitleTextBlock.Text = "Financial";
+            //Financial.IsSelected = true;
+
         }
 
         public CountryCodesVM ReadInFile { get; set; }
+       
+            
 
-
-        private async void WeatherButton_Click(object sender, RoutedEventArgs e)
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
-            var position = await LocationManager.GetPosition();
-
-            OpenWeatherRootObject myWeather = await WeatherFacade.GetWeatherLatlon(
-                position.Coordinate.Latitude, position.Coordinate.Longitude);
-
-            WeatherResultText.Text = myWeather.name.ToString() + " -- " + myWeather.weather[0].description;
-                
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
 
-        private async void WeatherCityButton_Click(object sender, RoutedEventArgs e)
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            var city = CityTextBox.Text.ToString();
-            var country = countryCombo.SelectedValue.ToString();
-
-            RootObject myCityWeather = await WeatherFacade.GetWeatherCity(country, city);
-
-            WeatherResultCityText.Text = myCityWeather.current_observation.display_location.city + " _  " + myCityWeather.current_observation.temp_c.ToString() + "-" + myCityWeather.current_observation.wind_kph;
-            
+            //if (MyFrame.CanGoBack)
+            //{
+            //    MyFrame.GoBack();
+            //    Financial.IsSelected = true;
+            //}
         }
 
-        //public string passTheCode()
-        //{
-            
-        //    return code;
-        //}
-
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Financial.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Collapsed;
+               // MyFrame.Navigate(typeof(Financial));
+                TitleTextBlock.Text = "Financial";
+            }
+            else if (Food.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+               // MyFrame.Navigate(typeof(Food));
+                TitleTextBlock.Text = "Food";
+            }
+        }
     }
+
+    //private async void WeatherButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    var position = await LocationManager.GetPosition();
+
+    //    OpenWeatherRootObject myWeather = await WeatherFacade.GetWeatherLatlon(
+    //        position.Coordinate.Latitude, position.Coordinate.Longitude);
+
+    //    WeatherResultText.Text = myWeather.name.ToString() + " -- " + myWeather.weather[0].description;
+
+    //}
+
+    //private async void WeatherCityButton_Click(object sender, RoutedEventArgs e)
+    //{
+    //    var city = CityTextBox.Text.ToString();
+    //    var country = countryCombo.SelectedValue.ToString();
+
+    //    RootObject myCityWeather = await WeatherFacade.GetWeatherCity(country, city);
+
+    //    WeatherResultCityText.Text = myCityWeather.current_observation.display_location.city + " _  " + myCityWeather.current_observation.temp_c.ToString() + "-" + myCityWeather.current_observation.wind_kph;
+
+    //}
+
+
 }
+
