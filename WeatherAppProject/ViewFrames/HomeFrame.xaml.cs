45042efad0f81f6cf4bf4d99a9052b4609e89f76
Sean McGrath <Sean McGrath>
2016-11-26 16:09:18 +0000

    added method to convert wind dierction to string

--- WeatherAppProject/ViewFrames/HomeFrame.xaml.cs
+++ WeatherAppProject/ViewFrames/HomeFrame.xaml.cs
@@ -31,8 +31,8 @@ namespace WeatherAppProject.ViewFrames
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            //MyProgressRing.IsActive = true;
-            //MyProgressRing.Visibility = Visibility.Visible;
+            MyProgressRing.IsActive = true;
+            MyProgressRing.Visibility = Visibility.Visible;
 
             //Makes call to a call to the location manger class to get geolocation
             var position = await LocationManager.GetPosition();
@@ -57,13 +57,37 @@ namespace WeatherAppProject.ViewFrames
             //Used to populate the xmal in Grid.Row 1
 
             WindressureResultText.Text = myWeather.wind.speed.ToString() + " kph";
+            
+            double degrees = myWeather.wind.deg;
+            var stringDegree = DegreesToCardinalDetailed(degrees);
+            WindDirectionResultText.Text = stringDegree.ToString();
 
-            double timestamp = myWeather.sys.sunrise;
+            //http://stackoverflow.com/questions/4505825/convert-utc-time-in-unix-time-format-to-a-readable-datetime-format
+            //reformates the rturned date and time from unix timestamp to Date time format
+            double timeStamp = myWeather.sys.sunrise;
             DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
-            dateTime = dateTime.AddSeconds(timestamp);
-            SunUpText.Text = dateTime.ToString();
-            //MyProgressRing.IsActive = false;
-            //MyProgressRing.Visibility = Visibility.Collapsed;
+            dateTime = dateTime.AddSeconds(timeStamp);
+            SunUpText.Text = dateTime.ToString() + " a.m";
+
+            //reformates the rturned date and time from unix timestamp to Date time format
+            timeStamp = myWeather.sys.sunset;
+            dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dateTime = dateTime.AddSeconds(timeStamp);
+            SunDownText.Text = dateTime.ToString() + " p.m";
+            WindImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+            MyProgressRing.IsActive = false;
+            MyProgressRing.Visibility = Visibility.Collapsed;
+        }
+
+        //this method was used from the below link, converts the degrees returned from the api to a string
+        //http://www.themethodology.net/2013/12/how-to-convert-degrees-to-cardinal.html
+        public static string DegreesToCardinalDetailed(double degrees)
+        {
+            degrees *= 10;
+
+            string[] caridnals = { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N" };
+            return caridnals[(int)Math.Round(((double)degrees % 3600) / 225)];
         }
     }
 }
