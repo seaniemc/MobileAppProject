248212c83baae82c6f0903d2fb6c9861769fba1a
Sean McGrath <Sean McGrath>
2016-11-29 21:53:47 +0000

    added forecast

--- WeatherAppProject/ViewFrames/ForecastFrame.xaml.cs
+++ WeatherAppProject/ViewFrames/ForecastFrame.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,10 +23,10 @@ namespace WeatherAppProject.ViewFrames
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
     public sealed partial class ForecastFrame : Page
     {
-        //public List<TxtForecast> txtForecast { get; set; }
-        public static List<TxtForecast> mainTxtForecast = new List<TxtForecast>();
+        public List<string> txtForecastList = new List<string>();
 
         public ForecastFrame()
         {
@@ -35,6 +36,8 @@ namespace WeatherAppProject.ViewFrames
 
         }
         public CountryCodesVM ReadInFile { get; set; }
+
+        string image = "";
 
         private async void ConditionsButton_Click(object sender, RoutedEventArgs e)
         {
@@ -46,6 +49,8 @@ namespace WeatherAppProject.ViewFrames
             else
             {
                 var city = CityTextBox.Text.ToString();
+                city = city.Replace(" ", "_");
+                var cityToUpper = Conditions.FirstCharToUpper(city);
                 var country = countryCombo.SelectedValue.ToString();
 
                 MyProgressRing.IsActive = true;
@@ -55,23 +60,70 @@ namespace WeatherAppProject.ViewFrames
 
                 //  myCityWeather.forecast.txt_forecast.date.ToString();
 
+                if (myCityWeather.forecast == null)
+                {
+                    CityTextBox.Text = "";
+                    CityTextBox.PlaceholderText = "Re-enter City Name";
+
+                    MyProgressRing.IsActive = false;
+                    MyProgressRing.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    //Day1Text.Text = day.title.ToString();
+                    foreach (var day in myCityWeather.forecast.txt_forecast.forecastday)
+                    {
+
+                        txtForecastList.Add(day.title.ToString());
+                        txtForecastList.Add(day.fcttext_metric.ToString());
+                        txtForecastList.Add(day.icon_url.ToString());
+
+                    }
+                    Day1Text.Text = txtForecastList[0].ToString();
+                    Forecast1Text.Text = txtForecastList[1].ToString();
+                    string icon = String.Format(txtForecastList[2].ToString());
+                    Weather1Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                    Day2Text.Text = txtForecastList[3].ToString();
+                    Forecast2Text.Text = txtForecastList[4].ToString();
+                    icon = String.Format(txtForecastList[5].ToString());
+                    Weather2Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                    Day3Text.Text = txtForecastList[6].ToString();
+                    Forecast3Text.Text = txtForecastList[7].ToString();
+                    icon = String.Format(txtForecastList[8].ToString());
+                    Weather3Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                    Day4Text.Text = txtForecastList[9].ToString();
+                    Forecast4Text.Text = txtForecastList[10].ToString();
+                    icon = String.Format(txtForecastList[11].ToString());
+                    Weather4Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                    Day5Text.Text = txtForecastList[12].ToString();
+                    Forecast5Text.Text = txtForecastList[13].ToString();
+                    icon = String.Format(txtForecastList[14].ToString());
+                    Weather5Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                    Day6Text.Text = txtForecastList[15].ToString();
+                    Forecast6Text.Text = txtForecastList[16].ToString();
+                    icon = String.Format(txtForecastList[17].ToString());
+                    Weather6Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                    Day7Text.Text = txtForecastList[18].ToString();
+                    Forecast7Text.Text = txtForecastList[19].ToString();
+                    icon = String.Format(txtForecastList[20].ToString());
+                    Weather7Image.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                    MyProgressRing.IsActive = false;
+                    MyProgressRing.Visibility = Visibility.Collapsed;
+
+                    CityTextBox.PlaceholderText = "Enter City Name";
+                    
+                }
+
             }
-
-            //foreach (var day in myCityWeather.forecast.txt_forecast.forecastday)
-            //{
-            //    day.title.ToString();
-            //}
-
+            
         }
-        //public static void CreateTxtForcast( forecastList)
-        //{
-        //    foreach (var item in forecastList)
-        //    {
-
-        //    }
-        //}
-    }
-
-        
-    
+       
+    }    
 }
